a84a45e38fb8ea3535e5b35f2ce37633d5500dcc
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2024-04-24 01:53:07 +0700

    Change logo & Theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css'
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import { json } from './types/surveys';
-import { ContrastDark } from "survey-core/themes/contrast-dark";
+import {  SolidLight } from "survey-core/themes/solid-light";
+// import { ThreeDimensionalLight } from "survey-core/themes/threedimensional-light";
 import "survey-core/defaultV2.min.css";
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
   const [pageNo, setPageNo] = React.useState<number>(0);
 
   React.useEffect(() => {
-    survey.applyTheme(ContrastDark);
+    survey.applyTheme(SolidLight);
+    
     setPageNo(survey.currentPageNo);
-  }, [survey]); // Apply theme only when survey object changes
+  }, [survey]); // Apply theme only when survey object changes 
 
   return (
     <Survey currentPageNo={pageNo} model={survey} />
